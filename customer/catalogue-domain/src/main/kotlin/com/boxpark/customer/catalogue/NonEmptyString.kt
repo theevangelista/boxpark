4ec05a4cdc43df136d0596ec57e55733f2a50bd5
Evangelista <evangelistajoaop@gmail.com>
2024-12-11 13:27:40 -0300

    add NonEmptyString
    Value to indicate a well-formed string

---
+++ customer/catalogue-domain/src/main/kotlin/com/boxpark/customer/catalogue/NonEmptyString.kt
@@ -0,0 +1,26 @@
+package com.boxpark.customer.catalogue
+
+import arrow.core.Either
+
+
+/**
+ * A [NonEmptyString] is a value that represents a non-empty string.
+ * A String is considered empty if there are no characters in it
+ * or if it contains only whitespace.
+ */
+@JvmInline
+value class NonEmptyString private constructor(val value: String) {
+    // EmptyString is a value that indicates that the string is empty,
+    // thus rejected.
+    data object EmptyString
+    companion object {
+
+        /**
+         * Create a [NonEmptyString] from a [value].
+         * @return [Either] [EmptyString] if the [value] is empty, [NonEmptyString] otherwise.
+         */
+        fun of(value: String): Either<EmptyString, NonEmptyString> =
+            if (value.isNotBlank()) Either.Right(NonEmptyString(value))
+            else Either.Left(EmptyString)
+    }
+}
